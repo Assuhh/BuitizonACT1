3851b21fe0b834e0b9b512824b504a5a5f563c33
Ace <buitizonacekarl@gmail.com>
2024-12-21 16:02:37 +0800

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -6,9 +6,4 @@ from .views import (HomePageView, AboutPageView, BlogListView,
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
-    path('blog/', BlogListView.as_view(), name='blog'),
-    path('blog/<int:pk>', BlogDetailView.as_view(), name='blog_detail'),
-    path('blog/create', BlogCreateView.as_view(), name='blog_create'),
-    path('blog/<int:pk>/edit', BlogUpdateView.as_view(), name='blog_update'),
-    path('blog/<int:pk>/delete', BlogDeleteView.as_view(), name='blog_delete'),
 ]
