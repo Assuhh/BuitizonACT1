4d9565ecdc97cde32753dc4f454b3405d0659dc7
Ace <buitizonacekarl@gmail.com>
2024-12-21 15:59:41 +0800

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,1 @@
 from django.contrib import admin
-from .models import User, Subject, Session, Payment, Review, Post
-
-admin.site.register(User)
-admin.site.register(Subject)
-admin.site.register(Session)
-admin.site.register(Payment)
-admin.site.register(Review)
-admin.site.register(Post)
